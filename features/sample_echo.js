eb33f330a08bbaa902fec80d1d7bb9fd983cdd83
Aaron Bruce <aaronbruce555@gmail.com>
2020-10-19 13:51:44 -0400

    Install mongoose and add a sample response to sample_echo.js

--- features/sample_echo.js
+++ features/sample_echo.js
@@ -9,6 +9,10 @@ module.exports = function(controller) {
         await bot.reply(message, 'I heard a sample message.');
     });
 
+    controller.hears('dia duit'.toLowerCase(), 'message,direct_message', async(bot, message) => {
+        await bot.reply(message, 'Dia is muire duit! Conas atá tú?');
+    })
+
     controller.on('message,direct_message', async(bot, message) => {
         await bot.reply(message, `Echo: ${ message.text }`);
     });
